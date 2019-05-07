c70cdb5f1eca08f7addaa9fdeaa2f4a671376df3
Nirmoy Das <ndas@suse.de>
2019-05-07 23:31:47 +0200

    print steps
    Signed-off-by: Nirmoy Das <ndas@suse.de>

--- namespace_network.sh
+++ namespace_network.sh
@@ -3,14 +3,18 @@ ip netns add test1
 ip netns add test2
 ls /var/run/netns
 
+echo creating veth interfaces to connect netns
 ip link add test1-eth0 type veth peer name test2-eth0
 
+echo moving veth interface to proper network namespace
 ip link set test1-eth0 netns test1
 ip link set test2-eth0 netns test2
 
+echo renaming interfaces to eth0 inside network namespace
 ip netns exec test1 ip link set test1-eth0 name eth0
 ip netns exec test2 ip link set test2-eth0 name eth0
 
+echo setting up interfaces inside netns
 ip netns exec test1 ip link set eth0 up 
 ip netns exec test1 ip link set lo up 
 
